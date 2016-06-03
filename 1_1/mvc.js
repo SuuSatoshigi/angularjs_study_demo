ceaf30c5b2433b95f4ba80463a6c4192a0938be1
Michelle M Y Zou <suusatoshigi@gmail.com>
2016-06-03 19:36:33 +0800

    add data 1_1 content

--- 1_1/mvc.js
+++ 1_1/mvc.js
@@ -1,5 +1,18 @@
-function angular($scope) {
-	$scope.greeting = {
-		text: 'hello'
-	};
-}
+var module = angular.module("HelloAngular",[]);
+var mymodule = angular.module("mymodule",[]);
+
+module.controller("angular",['$scope',
+	function HelloAngular($scope) {
+		$scope.greeting = {
+			text: 'hello'
+		};
+	}
+]);
+
+mymodule.directive("cordova",function() {
+	return {
+		restrict: 'E',
+		template: '<div>cordova study </div>',
+		replace: true
+	}
+});
